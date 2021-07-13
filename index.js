fbb83a18c772a92a58c59e2a2d9a8cc957a49fef
E3ddie <E3ddi33@outlook.com>
2021-07-13 16:02:07 +0300

    Update JS File

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-let count = 9
-let myAge = 30;
-console.log(myAge)
-console.log(count)
+// // let count = 9
+// // console.log(count)
 
+// // let myAge = 30;
+// // console.log(myAge)
+
+// // let counter = 5/7
+
+// //1 h = 7d
+// let myAge = 20
+// let humanR = 7
+// let myDogAge = myAge * humanR
+// console.log(myDogAge)
+
+let bonusPoints = 50
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+bonusPoints= bonusPoints + 45
+console.log(bonusPoints)
