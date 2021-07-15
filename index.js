d4c5d55550f30722f055a6a406b27c6fd73ae29d
E3ddie <E3ddi33@outlook.com>
2021-07-15 06:53:07 -0400

    JS changes

--- index.js
+++ index.js
@@ -12,10 +12,43 @@
 // let myDogAge = myAge * humanR
 // console.log(myDogAge)
 
-let bonusPoints = 50
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-bonusPoints= bonusPoints + 45
-console.log(bonusPoints)
+// let bonusPoints = 50
+// console.log (bonusPoints)
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+// bonusPoints= bonusPoints + 45
+// console.log(bonusPoints)
+
+// function increment() {
+//     console.log("Success!!")
+// }
+
+// function bell() {
+//     console.log(42)
+//     console.log("---")
+
+// }
+// bell()
+// bell()
+
+// let l1 = 30
+// let l2 = 55
+// let l3 = 70
+
+// function tots() {
+//     let tot = l1 + l2 + l3
+//     console.log(tot)
+// }
+
+// tots()
+
+let lapsComp = 0
+function laps() {
+    lapsComp += 1
+    console.log(lapsComp)
+}
+laps()
+laps()
+laps()
